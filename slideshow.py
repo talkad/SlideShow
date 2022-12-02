7df5a6f0fcfe0ac948e000fb7c8d652f37e3eaa8
Tal Kadosh <talkad@post.bgu.ac.il>
2022-12-02 23:43:04 +0200

    image fading

--- slideshow.py
+++ slideshow.py
@@ -2,41 +2,46 @@ from flask import Flask, Response
 import os
 import time
 
-app = Flask(__name__)
 
-image_folder = 'img'
+app = Flask(__name__, static_url_path='/static')
 
-def gen():
-    i = 0
-    images = get_all_images()
-
-    while True:
-        time.sleep(5) # slide duration
-        image_name = images[i]
-        img = open(os.path.join(image_folder, image_name), 'rb').read()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n')
-        i = (i+1) % len(images)
+image_folder = '/static/images'
 
 
 def get_all_images():
-    images = [img for img in os.listdir(image_folder)
+    images = [img for img in os.listdir(image_folder[1:])
               if img.endswith(".jpg") or
               img.endswith(".jpeg") or
               img.endswith("png")]
     return images
 
 
-@app.route('/slideshow')
-def slideshow():
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+def get_html():
+    '''
+    inject images into HTML
+    '''
+    code = ''
+    updated_code = ''
 
+    delimiter = '<div id="slides">'
+
+    with open('index.html', 'r') as f:
+        code = f.read()
+
+    code = code.split(delimiter)
+    updated_code = f'{code[0]}\n{delimiter}\n'
+
+    for img in get_all_images():
+        updated_code += f'<img src="{os.path.join(image_folder, img)}" class="slideShow" />\n'
+
+    updated_code += code[1]
+    return updated_code
+    
 
 @app.route('/')
 def index():
-    with open('index.html', 'r') as f:
-        return f.read()
+    return get_html()
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', debug=True)
